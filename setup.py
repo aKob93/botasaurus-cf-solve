adf4bf85943db0c031d4d13960c7a61a8f664f74
Aleksandr Kobelyov <akobelyov@gmail.com>
2025-01-30 14:09:22 +0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ def get_description():
 
 setup(
     name="botasaurus_cf_solve",
-    packages=find_packages(include=["botasaurus_cf_solve", "botasaurus_cf_solve.*"]),
+    packages=find_packages(include=["botasaurus_cf_solve", "botasaurus_cf_solve.*", "botasaurus_api", "close_chrome", "botasaurus_controls"]),
     version='4.0.60',
     license="MIT",
     project_urls={
